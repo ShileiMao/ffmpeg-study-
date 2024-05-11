36228d7f8b4bea043a78c1b371f55850e708a4eb
Shilei Mao <shilei.mao2018@gmail.com>
2024-05-11 11:35:53 +0100

    link ffmpeg library

--- build/CMakeFiles/3.29.3/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/shileimao/Documents/Programming\ Languages/C++/player/player/build/CMakeFiles/3.29.3/CompilerIdC/CMakeCCompilerId.c
--- build/build/player.build/Debug/Objects-normal/arm64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/shileimao/Documents/Programming\ Languages/C++/player/player/src/main.cpp \
-  /Users/shileimao/Documents/Programming\ Languages/C++/player/player/lib/glfw/include/GLFW/glfw3.h
